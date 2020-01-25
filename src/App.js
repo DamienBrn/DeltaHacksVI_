6e8e5e5dfa6575d651a1f481f5c887d279d40255
DamienBrn <damien.berno@outlook.com>
2020-01-25 21:56:45 +0100

    v1.0 - camera set up

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import './camera'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <div className="display-cover">
+            <video autoPlay></video>
+            <canvas className="d-none"></canvas>
+
+            <div className="video-options">
+                <select name="" id="" className="custom-select">
+                    <option value="">Select camera</option>
+                </select>
+            </div>
+
+            <img className="screenshot-image d-none" alt=""/>
+
+            <div className="controls">
+                <button className="btn btn-danger play" title="Play"><i data-feather="play-circle"></i></button>
+                <button className="btn btn-info pause d-none" title="Pause"><i data-feather="pause"></i></button>
+                <button className="btn btn-outline-success screenshot d-none" title="ScreenShot"><i data-feather="image"></i></button>
+            </div>
+          </div>
   );
 }
+
 
 export default App;
